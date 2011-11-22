70c6cc6622ed104a87816f1424bb8a728f8e6fd9
Ladislav Solc <ladislav.solc@gmail.com>
2011-11-22 08:30:26 +0100

    Basic CSS layout

--- application.js
+++ application.js
@@ -1,24 +1,18 @@
 
 
 $(document).ready(function() {
-	$.getJSON("data.json", function(json){		//	$.getJSON("data.js", function(json){		
-
-		var x=json.length;
-		console.log("Records: " + x);			
-		for(i=0;i<x;i++){
-
-			$.each(json[i], function(key, val) {
-					
-				var template = '<li class="item">{{key}}: {{val}}</li>';			
-				$("<li><strong>" + key + ": </strong>"+ val + "</li>").appendTo("#list");	//	$("<li><a href='http://" + val + "'>" + val + "</a></li>").appendTo("#list");
-				$("strong").css("color","blue");
+	$.getJSON("data.json", function(data){
+		var templateText = $("#entry-template").html();
+		var template = Handlebars.compile(templateText);
+				
+		var htmlString = template({log_items : data});
+		console.log(htmlString);
+		$('.log_list').html(htmlString);
+	});
+});
 
 
-				}); // each()
-				console.log("jskjsjsjjss");
 
-				$("<br></br>").appendTo("ul#list");
-
-			}
-			}); // .getJSON()
-			}); // ready()
+/*
+var o = {};
+		o.logItems = data;*/
