6616b30f441c9977151bf3efbd7ab583b22f567b
Ladislav Solc <ladislav.solc@gmail.com>
2011-11-29 15:14:46 +0100

    Miner CSS modifications

--- application.js
+++ application.js
@@ -1,15 +1,28 @@
-
-
 $(document).ready(function() {
 	$.getJSON("data.json", function(data){
 		var templateText = $("#entry-template").html();
 		var template = Handlebars.compile(templateText);
 				
 		var htmlString = template({log_items : data});
-		console.log(htmlString);
+		//console.log(htmlString);
 		$('.log_list').html(htmlString);
+		
+		$("div#button_header").click(function() {									
+			var header = $(this);
+			var detail = header.parent().find('.content');
+			detail.toggleClass("hidden");			
+		});		
 	});
 });
+
+
+
+
+
+
+
+
+
 
 
 
